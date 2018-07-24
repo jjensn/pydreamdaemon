aa2fb93cbd2d8e55e431b20f06ac019c5f4605d8
peeprs <peeprs@peeprss-air.attlocal.net>
2018-07-24 18:26:57 -0400

    Working OK

--- yedream/__init__.py
+++ yedream/__init__.py
@@ -1,4 +1,4 @@
-import logging, yaml
+import logging, yaml, copy
 from yeelight import Bulb
 
 from colormath.color_objects import sRGBColor, LabColor
@@ -7,7 +7,7 @@ from colormath.color_diff import delta_e_cie2000
 import threading
 
 state = { 
-    'power': 'off',
+    'power': 'on',
     'music_mode': 0,
     'colors': {
         'r': 0,
@@ -37,9 +37,9 @@ class YeDream:
 
         for light in self._settings["bulbs"]:
             light["state"] = state
-            light["object"] = Bulb(light["ip"], port=55443, effect=self._settings["settings"]["effect"], duration=self._settings["settings"]["duration"], auto_on=False)
+            self._bulbs.append(Bulb(light["ip"], port=55443, effect=self._settings["settings"]["effect"], duration=self._settings["settings"]["duration"], auto_on=False))
 
-        self._init_bulbs(self._settings["bulbs"])
+        self._init_bulbs(self._bulbs)
         self.project()
 
     def __exit__(self, *args):
@@ -59,28 +59,28 @@ class YeDream:
             exit(2)
 
     def _init_bulbs(self, bulbs):
-        for light in bulbs:
-            yeelight = light.get("object")
+        for idx,light in enumerate(bulbs):
+            yeelight = light
             yeelight.get_properties()
-            light.get("state")["power"] = yeelight.last_properties.get("power")
-            light.get("state")["brightness"] = yeelight.last_properties.get("brightness")
+            self._settings["bulbs"][idx]["state"]["power"] = yeelight.last_properties.get("power")
+            # light.get("state")["brightness"] = yeelight.last_properties.get("brightness")
 
             rgb = int(yeelight.last_properties.get("rgb"))
             blue = rgb & 0xff
             green = (rgb >> 8) & 0xff
             red = (rgb >> 16) & 0xff
 
-            light.get("state")["colors"]["r"] = red
-            light.get("state")["colors"]["g"] = green
-            light.get("state")["colors"]["b"] = blue
+            self._settings["bulbs"][idx]["state"]["colors"]["r"] = red
+            self._settings["bulbs"][idx]["state"]["colors"]["g"] = green
+            self._settings["bulbs"][idx]["state"]["colors"]["b"] = blue
 
             yeelight.start_music()
-            light.get("state")["music_mode"] = 1
-
+            self._settings["bulbs"][idx]["state"]["music_mode"] = 1
+            brightness_thread = threading.Thread(target=light.set_brightness, args=[100])  # <- 1 element list
+            brightness_thread.start()
+        
     def project(self):
         """Set lights based on the subscription data."""
-
-        c = self._settings
         zones = self.zone_data
 
         if zones == None:
@@ -88,17 +88,18 @@ class YeDream:
             threading.Timer(5, self.project).start()
             return
 
-        #threading.Timer(float(c["settings"]["update_rate"]), self.project).start()
         threading.Timer(.25, self.project).start()
-        # for zone, colors in zones.items():
-        for bulb in c["bulbs"]:
 
+        # threading.Timer(float(self._settings["settings"]["update_rate"]), self.project).start()
+        settings_copy = copy.deepcopy(self._settings)
+        for idx, bulb in enumerate(settings_copy["bulbs"]):
+            # print ("%s %s", bulb["name"], bulb.get("state")["power"])
             zone_count = 0
             r = 0
             b = 0
             g = 0
 
-            for target_zone in bulb["zones"]:
+            for target_zone in list(bulb["zones"]):
                 zone_count = zone_count + 1
                 zone = self.zone_data.get(int(target_zone - 1))
                 r = r + zone['r']
@@ -110,44 +111,67 @@ class YeDream:
                 g = int(g / zone_count)
                 b = int(b / zone_count)
 
-            self._logger.info("Average zone color for bulb %s: [ %i %i %i ]", bulb["name"], r, g, b)
+            # self._logger.info("Average zone color for bulb %s: [ %i %i %i ]", bulb["name"], r, g, b)
 
-            if r + b + g >= 25:
+            if r + b + g >= 10:
+                
+                # if bulb.get("state")["power"] == "off":
+                #     self._settings["bulbs"][idx]["state"]["power"] = "on"
+                #     # self._bulbs[idx].get_properties()
+                #     # if self._bulbs[idx].last_properties["power"] == "off":
+                #     # bulb.get("object").turn_on()
+                #     self._logger.info("Turning on %s", bulb["name"])
+                #     # bulb.get("object").set_brightness(self._settings["settings"]["max_brightness"])
+                #     # brightness_thread = threading.Thread(target=self._bulbs[idx].set_brightness, args=[int(self._settings["settings"]["max_brightness"])])  # <- 1 element list
+                #     # brightness_thread = threading.Thread(target=self._bulbs[idx].set_brightness, args=[100])  # <- 1 element list
+                #     # brightness_thread.start()
+                    
+                #     # bulb.get("object").turn_on()
+                #     continue
+
                 curr_colors = bulb.get("state")["colors"]
+
                 curr_rgb = sRGBColor(curr_colors["r"], curr_colors["g"], curr_colors["b"], True)
-                # self._logger.debug("r g b %f %f %f", old_red, old_green, old_blue)
-                # Blue Color
                 new_rgb = sRGBColor(r, g, b, True)
-                # self._logger.debug("r g b %f %f %f", r, g, b)
 
                 curr_lab = convert_color(curr_rgb, LabColor)
                 new_lab = convert_color(new_rgb, LabColor)
 
                 delta = delta_e_cie2000(curr_lab, new_lab)
-                self._logger.info("Delta for zone %s: [ %i ]", bulb["name"], delta)
-                if delta >= c["settings"]["sensitivity"]:
-                    # if bulb.get("state")["power"] == "off":
-                        # bulb.get("object").turn_on()
-                        # bulb.get("object").set_brightness(c["settings"]["max_brightness"])
-                        # bulb.get("object").turn_on()
-                    bulb.get("state")["power"] = "on"
-
-                    bulb.get("state")["colors"]["r"] = r
-                    bulb.get("state")["colors"]["g"] = g
-                    bulb.get("state")["colors"]["b"] = b
-                    self._logger.info("Changing zone %s: [ %i ]", bulb["name"], delta)
-                    processThread = threading.Thread(target=bulb.get("object").set_rgb, args=[r, g, b])  # <- 1 element list
-                    processThread.start()
+                # self._logger.info("Delta for zone %s: [ %i ]", bulb["name"], delta)
+                if delta >= self._settings["settings"]["sensitivity"]:
+                    self._settings["bulbs"][idx]["state"]["power"] = "on"
+                    self._settings["bulbs"][idx]["state"]["colors"]["r"] = r
+                    self._settings["bulbs"][idx]["state"]["colors"]["g"] = g
+                    self._settings["bulbs"][idx]["state"]["colors"]["b"] = b
+                    # self._logger.info("Changing zone %s: [ %i ]", bulb["name"], delta)
+                    # brightness_thread = threading.Thread(target=self._bulbs[idx].turn_on) #, args=[int(self._settings["settings"]["max_brightness"]])) 
+                    # brightness_thread.start()
+                    self._bulbs[idx].set_brightness(100)
+                    color_thread = threading.Thread(target=self._bulbs[idx].set_rgb, args=[r, g, b])  # <- 1 element list
+                    color_thread.start()
 
                     # bulb.get("object").set_rgb(r, g, b)
 
-            # else:
+            else:
+                # if bulb["state"]["power"] == "on":
+
+                    # brightness_thread = threading.Thread(target=self._bulbs[idx].set_brightness, args=[1])  # <- 1 element list
+                # brightness_thread = threading.Thread(target=self._bulbs[idx].turn_off) #, args=[1])  # <- 1 element list
+                # brightness_thread.start()
                 # if bulb.get("state")["power"] == "on":
-                    # bulb.get("object").turn_off()
-                # bulb.get("state")["power"] = "off"
+                    # self._settings["bulbs"][idx]["state"]["power"] = "off"
+                    # self._settings["bulbs"][idx]["state"]["power"] = "off"
+                # self._logger.info("Turning off %s", bulb["name"])
+                self._bulbs[idx].set_brightness(1)
+                    # brightness_thread = threading.Thread(target=self._bulbs[idx].set_brightness, args=[1])  # <- 1 element list
+                    # brightness_thread.start()
+                    # bulb.get("object").turn_on()
+                    
+                    # self._settings["bulbs"][idx]["state"]["power"] = "off"
 
             # for bulb in c.config.bulbs:
-
+        
             
             # if id == 0:
 
